29f30d4de3971069a45f5ee8dd0629c1ca5ad6bf
ozgeergezer <ozgeergezerrr@gmail.com>
2021-12-24 14:54:28 +0300

    241221

--- EntityLayer/Concreate/Heading.cs
+++ EntityLayer/Concreate/Heading.cs
@@ -13,7 +13,6 @@ namespace EntityLayer.Concreate
         public int HeadingId { get; set; }
         public string HeadingName { get; set; }
         public DateTime HeadingDate { get; set; }
-
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
         public int WriterId { get; set; }
