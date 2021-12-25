5213fc1c8604561669f2a75c9d88df24c09a3fa6
Özge Ergezer <ozgeergezerrr@gmail.com>
2021-12-25 21:05:42 +0300

    251221

---
+++ BusinessLayer/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concreate.Repositories;
+using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo=new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBL(Category p)
+        {
+            if (p.CategoryName==""|| p.CategoryName.Length<=3||p.CategoryDescription=="" || p.CategoryDescription.Length<=51)
+            {
+                //hata
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T> //crud yapacağız burada
+    {
+        List<T> List();
+        void Insert(T p);
+        void Update(T p);
+        void Delete(T p);
+    }
+}
