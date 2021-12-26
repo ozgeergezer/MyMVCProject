4da8864ff9ba131e900b4b5cf9e4f2adeb4140a1
Özge Ergezer <ozgeergezerrr@gmail.com>
2021-12-26 21:05:44 +0300

    261221

--- DataAccessLayer/Abstract/IRepository.cs
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,6 @@ namespace DataAccessLayer.Abstract
         void Insert(T p);
         void Update(T p);
         void Delete(T p);
+        List<T> List(Expression<Func<T, bool>> filter);
     }
 }
--- MyMVCProject/Controllers/CategoryController.cs
+++ MyMVCProject/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,15 @@ namespace MyMVCProject.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager cm=new CategoryManager(); //bldeki sınıfı çağırma
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetAllBL();  //blden oluşan sınıfı çağırmam lazım
+            return View(categoryvalues);
         }
     }
 }
